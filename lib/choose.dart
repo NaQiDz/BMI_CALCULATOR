1b17986e8e32da8751f6c95cddc23a39f55e3a79
Ahmad Naqiuddin <ahmadnaqiuddinmohamad@gmail.com>
2024-11-10 13:12:36 +0800

    progress2

---
+++ lib/choose.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'started.dart';
+import 'login.dart';
+
+void main() {
+  runApp(ChoosePage());
+}
+
+class ChoosePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.black, Colors.blue.shade900],
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start, // Aligns to the top
+              children: [
+                SizedBox(height: 200), // Adds 100 pixels of space from the top
+                Image.asset(
+                  'sources/img1.png',
+                  height: 400,
+                  width: 400,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Welcome to',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  'SihatSelalu App',
+                  style: TextStyle(
+                    color: Colors.blue.shade500,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  '\nAlready has an account?\nSign up now!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => StartedPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade600,
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign up for free',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  child: Text(
+                    'Log in',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
