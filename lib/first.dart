47f86f0f19d501cad0bcf0e3eb894b54f6b4fdab
Ahmad Naqiuddin <ahmadnaqiuddinmohamad@gmail.com>
2024-11-10 13:12:45 +0800

    progress2

---
+++ lib/first.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'second.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to SecondPage
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+          child: Text('Go to Second Page'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/second.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to FirstPage
+            Navigator.pop(context);
+          },
+          child: Text('Go back'),
+        ),
+      ),
+    );
+  }
+}
